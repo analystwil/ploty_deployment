d719b4e41cc42d2564e8d6ba403059a70f40c877
Ashley W <77358388+analystwil@users.noreply.github.com>
2021-05-10 21:16:06 -0400

    Delete SpaceX directory

--- SpaceX/spaceX.js
+++
@@ -1,6 +0,0 @@
-//In the first line, the URL to the SpaceX is defined.
-
-const url = "https://api.spacexdata.com/v2/launchpads";
-
-//In the second line, d3.json() method places a call to SpaceX's API, retrieves the data, and prints it to the browser console.
-d3.json(url).then(receivedData => console.log(receivedData));
