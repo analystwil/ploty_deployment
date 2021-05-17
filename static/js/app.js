6925fa5da2b661e1429b94ddbd89f7212f26f752
analystwil <ashley.gibson12@gmail.com>
2021-05-16 21:18:50 -0400

    challenege complete

---
+++ static/js/app.js
@@ -0,0 +1,38 @@
+// FUNCTION #1 of 4
+function buildCharts(patientID) {
+    d3.json("samples.json").then(data => {
+        console.log(data)
+    })
+};
+
+// FUNCTION #2 of 4
+function populateDemoInfo(patientID) {
+
+    var demographicInfoBox = d3.select("#sample-metadata");
+
+    d3.json("samples.json").then(data => {
+        console.log(data)
+    })
+}
+
+// FUNCTION #3 of 4
+function optionChanged(patientID) {
+    console.log(patientID);
+    buildCharts(patientID);
+    populateDemoInfo(patientID);
+}
+
+// FUNCTION #4 of 4
+function initDashboard() {
+    var dropdown = d3.select("#selDataset")
+    d3.json("samples.json").then(data => {
+        var patientIDs = data.names;
+        patientIDs.forEach(patientID => {
+            dropdown.append("option").text(patientID).property("value", patientID)
+        })
+        buildCharts(patientIDs[0]);
+        populateDemoInfo(patientIDs[0]);
+    });
+};
+
+initDashboard();
